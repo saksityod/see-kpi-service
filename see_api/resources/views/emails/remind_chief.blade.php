3d412c3ce6f14e2888d1d35d3c32dc9bad2f0b04
Dome <msuksang@gmail.com>
2018-03-08 18:25:34 +0700

    Fixed mail template

--- see_api/resources/views/emails/remind_chief.blade.php
+++ see_api/resources/views/emails/remind_chief.blade.php
@@ -16,25 +16,27 @@ th, td {
 <p>กรุณาคลิก Link เพื่อไปเพิ่ม Action Plan สำหรับ KPI ที่ต่ำกว่าเกณฑ์</p>
 
 @foreach ($items as $k => $item)
-
-	<p>KPI ของคุณ {{$k}}</p>
-	<table>
-		<tr>
-			<th>Item Name</th>
-			<th>Target Value</th>
-			<th>Actual Value</th>
-			<th>Action Plan</th>
-		</tr>
-		@foreach ($item['items'] as $i)
-		<tr>
-			<td>{{$i->item_name}}</td>
-			<td>{{$i->target_value}}</td>
-			<td>{{$i->actual_value}}</td>
-			<td><a href="{{$web_domain}}kpi-result?param_link=email&param_item_result_id={{$i->item_result_id}}">{{$web_domain}}kpi-result?param_link=email&param_item_result_id={{$i->item_result_id}}</a></td>
-		</tr>
-		@endforeach
-	</table>
-	<br/>
+	@if ($k == 'chief_name')
+	@else
+		<p>KPI ของคุณ {{$k}}</p>
+		<table>
+			<tr>
+				<th>Item Name</th>
+				<th>Target Value</th>
+				<th>Actual Value</th>
+				<th>Action Plan</th>
+			</tr>
+			@foreach ($item['items'] as $i)
+			<tr>
+				<td>{{$i->item_name}}</td>
+				<td>{{$i->target_value}}</td>
+				<td>{{$i->actual_value}}</td>
+				<td><a href="{{$web_domain}}kpi-result?param_link=email&param_item_result_id={{$i->item_result_id}}">{{$web_domain}}kpi-result?param_link=email&param_item_result_id={{$i->item_result_id}}</a></td>
+			</tr>
+			@endforeach
+		</table>
+		<br/>
+	@endif
 @endforeach
 
 <p>From SEE-KPI System</p>
