911e26821464c9c7a3921cddc5298d7f39694198
Dome <msuksang@gmail.com>
2018-04-05 17:23:33 +0700

    Fixed email status link to separate between assignment and kpi result base on current stage

--- see_api/resources/views/emails/status.blade.php
+++ see_api/resources/views/emails/status.blade.php
@@ -2,7 +2,11 @@ To {{$chief_emp_name}}
 <br/>
 <br/>
 	<p>ข้อมูลของคุณ {{$emp_name}} อยู่ที่สถานะ "{{$status}}" กรุณา Click Link
+	@if ($assignment_flag == 1)
 	<a href="{{$web_domain}}assignment-popup/?emp_result_id={{$emp_result_id}}&appraisal_type_id={{$appraisal_type_id}}">{{$web_domain}}assignment-popup/?emp_result_id={{$emp_result_id}}&appraisal_type_id={{$appraisal_type_id}}</a>
+	@else
+	<a href="{{$web_domain}}kpi-result-popup/?emp_result_id={{$emp_result_id}}&appraisal_type_id={{$appraisal_type_id}}">{{$web_domain}}kpi-result-popup/?emp_result_id={{$emp_result_id}}&appraisal_type_id={{$appraisal_type_id}}</a>	
+	@endif
 	ระบบเพื่อทำการประเมิน</p>
 <br/>
 <br/>
