96699fe6064a6b37062889cf34baf734bfe3d2db
gj-toto <thawatchai@goingjesse.com>
2018-06-28 09:59:10 +0700

    fixed assignment flag

--- see_api/app/Http/Controllers/AppraisalAssignmentController.php
+++ see_api/app/Http/Controllers/AppraisalAssignmentController.php
@@ -1165,7 +1165,7 @@ class AppraisalAssignmentController extends Controller
 					and er.level_id = al.level_id
 					and er.stage_id = ast.stage_id
 					and ast.appraisal_type_id = 2
-					and ast.assignment_flag = 1
+					#and ast.assignment_flag = 1
 					".$emp_level."
 					".$org_level."
 					".$org_id."
