5725ff7cf6680b5ea04941ae02e86d7f4d43fdd3
NadaComsci <34049504+NadaComsci@users.noreply.github.com>
2018-11-23 18:36:21 +0700

    [Edited]: Service and Report Appraisal Statistics
    - แก้ชื่อบริษัทให้นำชื่อมาจากฐานข้อมูล
    - แก้ไขสูตรการคำนวณ STD
    - เพิ่ม grand total

--- see_api/app/Http/Controllers/Bonus/BonusReportController.php
+++ see_api/app/Http/Controllers/Bonus/BonusReportController.php
@@ -42,16 +42,25 @@ class BonusReportController extends Controller
         , oo.org_name as parent_org_name
         , o.org_id, o.org_code, o.org_name
         , (SELECT appraisal_year FROM appraisal_period WHERE period_id = ".$period_id.") as appraisal_year
+        , name_office.org_name as name_company
         FROM org oo
         -- LEFT JOIN org o ON o.parent_org_code = oo.org_code
         LEFT JOIN (
 					 SELECT o.org_id, o.org_code, o.org_name, o.level_id, o.parent_org_code
 					 FROM org o
-					 INNER JOIN (SELECT org_id FROM emp_result GROUP BY org_id) e_o ON e_o.org_id = o.org_id
+					 INNER JOIN (SELECT org_id FROM emp_result WHERE period_id = ".$period_id." GROUP BY org_id) e_o
+            ON e_o.org_id = o.org_id
 				) o ON o.parent_org_code = oo.org_code
         INNER JOIN appraisal_level le ON oo.level_id = le.level_id
+        CROSS JOIN (
+            SELECT o.org_name FROM org o
+            INNER JOIN appraisal_level le ON o.level_id = le.level_id
+            ORDER BY le.seq_no ASC, o.org_id ASC
+            LIMIT 1
+				) name_office
         WHERE le.is_start_cal_bonus = 1
         AND FIND_IN_SET(oo.org_id, '".$bu_org_id."')
+        AND o.org_id IS NOT NULL
         ORDER BY oo.org_code ASC, o.org_code ASC");
 
       $Query_Information_Score = "
@@ -87,7 +96,8 @@ class BonusReportController extends Controller
         SELECT round(result.min,2) as min
         , round(result.max,2) as max
         , round(result.difference,2) as difference
-        , round(avg(result.score_std),2) as std
+        -- , round(avg(result.score_std),2) as std
+        , round(SQRT(sum(result.score_std)/(count(result.score_std)-1)),2) as std
         , round(result.mean,2) as mean
         , round(avg(case when result.odd = 1
         then
@@ -112,7 +122,7 @@ class BonusReportController extends Controller
             , valuee.mean
             , round(valuee.position_median,0) as position_median
             , valuee.odd
-            , (re.score-valuee.mean) as score_std
+            , pow((re.score-valuee.mean),2) as score_std
             FROM
             (
               SELECT er.emp_result_id as emp_result_judgement_id
@@ -200,6 +210,15 @@ class BonusReportController extends Controller
         AND pe.appraisal_year = ?
         AND em.appraisal_type_id = 2 ";
 
+      $Total_Before_After = "
+          SELECT round(sum(em.b_amount)/sum(em.net_s_amount),2) as before_bonus
+          , round(sum(em.adjust_b_amount)/sum(em.net_s_amount),2) as after_bonus
+          FROM emp_result em
+          INNER JOIN appraisal_period pe ON em.period_id = pe.period_id
+          WHERE pe.period_id = ?
+          AND pe.appraisal_year = ?
+          AND em.appraisal_type_id = 2 ";
+
 
       foreach ($AllOrg as $org) {
 
@@ -256,7 +275,7 @@ class BonusReportController extends Controller
         }
 
         // หาค่าของข้อมูล ผจก.BU ตาม parent_org_id
-        $org->ValueManagerBU = $this->ValueManagerOrg($org->parent_org_id, $period_id, $appraisal_year);
+        $org->ValueManagerBU = $this->ValueManagerOfOrg($org->parent_org_id, $period_id, $appraisal_year);
 
         // คำนวนค่าโบนัสก่อนแก้ไข และหลังแก้ไข ตาม parent_org_id
         $ParentBefore_After = DB::select($Before_After_Score
@@ -312,7 +331,7 @@ class BonusReportController extends Controller
         }
 
         // หาค่าของข้อมูล ผจก.ฝ่าย ตาม org_id
-        $org->ValueManagerDivision = $this->ValueManagerOrg($org->org_id, $period_id, $appraisal_year);
+        $org->ValueManagerDivision = $this->ValueManagerOfOrg($org->org_id, $period_id, $appraisal_year);
 
         // คำนวนค่าโบนัสก่อนแก้ไข และหลังแก้ไข ตาม org_id
         $Before_After = DB::select($Before_After_Score
@@ -332,6 +351,16 @@ class BonusReportController extends Controller
 
         foreach ($Count_Staff as $CountStaff) {
             $org->CountStaffDivision = $CountStaff->num_emp;
+        }
+
+        // --------------------------Grand Total-------------------------- //
+        // หา before, after ของทุกคนในบริษัท (รวมทุก org)
+        $TotalBeforeAfter = DB::select($Total_Before_After
+          ,array($period_id, $appraisal_year));
+
+        foreach ($TotalBeforeAfter as $Total) {
+            $org->Total_Before = $Total->before_bonus;
+            $org->Total_After = $Total->after_bonus;
         }
 
       } //end foreach -> $AllOrg
@@ -359,40 +388,39 @@ class BonusReportController extends Controller
 
       // return ($AllOrg);
 
-      $now = new DateTime(); 
-      $date = $now->format('Y-m-d_H-i-s');   
-      $data = json_encode($AllOrg);
-      $namefile = 'BonusReportController_'.$date;  
+      $now = new DateTime();
+      $date = $now->format('Y-m-d_H-i-s');
+      $data = json_encode($AllOrg);             // ใช้ $AllOrg เป็นข้อมูลภายในไฟล์ .JSON
+      $namefile = 'BonusReportController_'.$date;
       $fileName = $namefile.'.json';
-      File::put(base_path("resources/generate/").$fileName,$data);  // สร้างไฟล์ JSON บันทึกข้อมูลลง JSON ไฟล์
-      if(file_exists(base_path("resources/generate/".$fileName))){  // ตรวจสอบไฟล์นั้นถูกสร้างสำเร็จแล้วยัง ? 
-        return response()->json(['status' => 200, 'data' => $namefile]);  // ส่งชื่อ JSON ไฟล์ไปยัง Front 
-       }else{ 
-        return response()->json(['status' => 400, 'data' => 'Generate File Not Success!']); 
+      File::put(base_path("resources/generate/").$fileName,$data);      // สร้างไฟล์ JSON บันทึกข้อมูลลง JSON ไฟล์
+      if(file_exists(base_path("resources/generate/".$fileName))){      // ตรวจสอบว่าไฟล์ .JSON ได้มีการสร้างขึ้นรึยัง?
+        return response()->json(['status' => 200, 'data' => $namefile]);      // ส่งชื่อไฟล์ .JSON ไปยัง Front
+       }else{
+        return response()->json(['status' => 400, 'data' => 'Generate File Not Success!']);
       }
 
 
       // รายงานสถิติวิเคราะห์สำหรับการประเมินผลงาน ประจำปี 20xx (บริษัท ดี เอช เอ สยามวาลา จำกัด)
       // คำนวณตาม org_id ของแต่ละ record
-      //  •	ผจก.ฝ่าย (คำนวณ) : ค่าข้อมูลคนที่มี appraisal_level.seq_no น้อยที่สุด และมี emp_id น้อยที่สุด
-      //  •	ผจก.BU (คำนวณ) : ค่าข้อมูลคนที่มี appraisal_level.seq_no น้อยที่สุด และมี emp_id น้อยที่สุด
-      //  •	Staff (จำนวน) : จำนวนคนทั้งหมดที่ไม่ใช่ ผจก.ฝ่าย และผจก.BU
-      //  •	ผจก.ฝ่าย (จำนวน) : นับจำนวนคนที่มี appraisal_level.seq_no น้อยที่สุด และมี emp_id น้อยที่สุด 
-      //  •	ผจก.BU (จำนวน) : นับจำนวนคนที่มี appraisal_level.seq_no น้อยที่สุด และมี emp_id น้อยที่สุด
-      //  •	ก่อนแก้ไข (Bonus) : sum(b_amount)/sum(net_s_amount)
-      //  •	หลังแก้ไข (Bonus) : sum(adjust_b_amount)/sum(net_s_amount)
-      //
+      //   •	ผจก.ฝ่าย (คำนวณ) : ค่าข้อมูลคนที่มี appraisal_level.seq_no น้อยที่สุด และมี emp_id น้อยที่สุด
+      //   •	ผจก.BU (คำนวณ) : ค่าข้อมูลคนที่มี appraisal_level.seq_no น้อยที่สุด และมี emp_id น้อยที่สุด
+      //   •	Staff (จำนวน) : จำนวนคนทั้งหมดที่ไม่ใช่ ผจก.ฝ่าย และผจก.BU
+      //   •	ผจก.ฝ่าย (จำนวน) : นับจำนวนคนที่มี appraisal_level.seq_no น้อยที่สุด และมี emp_id น้อยที่สุด
+      //   •	ผจก.BU (จำนวน) : นับจำนวนคนที่มี appraisal_level.seq_no น้อยที่สุด และมี emp_id น้อยที่สุด
+      //   •	ก่อนแก้ไข (Bonus) : sum(b_amount)/sum(net_s_amount)
+      //   •	หลังแก้ไข (Bonus) : sum(adjust_b_amount)/sum(net_s_amount)
       // คำนวณตาม org_id ของลูกหลานตามแต่ละ record
-      //  •	Min
-      //  •	Max
-      //  •	ผลต่าง : Max-Min
-      //  •	STD : [sum (ข้อมูลดิบ-Mean)] / จำนวนข้อมูลทั้งหมด
-      //  •	Mean : avg
-      //  •	Median (ค่ากลาง) : [ตำแหน่ง : (จำนวนข้อมูลทั้งหมด+1)/2] : นำตำแหน่งไปหาข้อมูล ณ ตำแหน่งนั้น
-      //   หากตำแหน่งเป็น 2.5 ให้นำข้อมูลตำแหน่ง 2 บวกข้อมูลตำแหน่ง 3 แล้วนำมาหาร 2
-      //  •	Mode : ข้อมูลที่ซ้ำกันมากที่สุด หากไม่มีข้อมูลที่ซ้ำกันจะถือว่าไม่มีฐานนิยม
-      //  •	ฝ่าย (คำนวณ) : avg(org_result_judgement.adjust_result_score)
-      //  •	BU (คำนวณ) : avg(org_result_judgement.adjust_result_score)
+      //   •	Min
+      //   •	Max
+      //   •	ผลต่าง : Max-Min
+      //   •	STD (ส่วนเบี่ยงเบนมาตรฐาน): {sum [(ข้อมูลดิบ-Mean)ยกกำลังสอง]}/(จำนวนข้อมูลทั้งหมด-1)
+      //   •	Mean : avg
+      //   •	Median (ค่ากลาง) : [ตำแหน่ง : (จำนวนข้อมูลทั้งหมด+1)/2] : นำตำแหน่งไปหาข้อมูล ณ ตำแหน่งนั้น
+      //      หากตำแหน่งเป็น 2.5 ให้นำข้อมูลตำแหน่ง 2 บวกข้อมูลตำแหน่ง 3 แล้วนำมาหาร 2
+      //   •	Mode : ข้อมูลที่ซ้ำกันมากที่สุด หากไม่มีข้อมูลที่ซ้ำกันจะถือว่าไม่มีฐานนิยม
+      //   •	ฝ่าย (คำนวณ) : avg(org_result_judgement.adjust_result_score)
+      //   •	BU (คำนวณ) : avg(org_result_judgement.adjust_result_score)
 
     }
 
@@ -484,6 +512,7 @@ class BonusReportController extends Controller
         AND pe.period_id = ".$period_id."
         AND pe.appraisal_year = ".$appraisal_year."
         AND emp.appraisal_type_id = 2
+        AND emp.emp_id IS NOT NULL
         GROUP BY le.seq_no
         ORDER BY le.seq_no ASC
         LIMIT 1");
@@ -503,6 +532,7 @@ class BonusReportController extends Controller
         	AND pe.appraisal_year = ".$appraisal_year."
         	AND le.seq_no = ".$min_seq[0]->seq_no."
         	AND emp.appraisal_type_id = 2
+          AND emp.emp_id IS NOT NULL
           LIMIT 1";
 
         $manager = DB::select("
@@ -524,7 +554,7 @@ class BonusReportController extends Controller
 
     }
 
-    public function ValueManagerOrg($org_id, $period_id, $appraisal_year){
+    public function ValueManagerOfOrg($org_id, $period_id, $appraisal_year){
 
       //$org_id, $period_id, $appraisal_year
 
@@ -544,10 +574,9 @@ class BonusReportController extends Controller
     		WHERE emp.org_id = ".$org_id."
     		AND pe.period_id = ".$period_id."
     		AND pe.appraisal_year = ".$appraisal_year."
+        AND emp.emp_id IS NOT NULL
     		AND emp.appraisal_type_id = 2 ";
-
       // return response()->json($min_seq);
-
 
       // หาจำนวนคนที่มีใน org นั้น
       $Count = DB::select("
@@ -556,19 +585,23 @@ class BonusReportController extends Controller
 
       // return response()->json($Count);
 
+
       if($Count[0]->num_emp == 0){
         $EmpManagerID = NULL;
       }else if ($Count[0]->num_emp == 1){
 
+        // หา emp_id ของคนที่เป็น ผจก.
         $EmpInOrg = DB::select($main_query);
         $EmpManagerID = $EmpInOrg[0]->emp_id;
 
       }else {
 
+          // หา seq_no ที่น้อยสุดสำหรับ org นี้
           $MinSeqNO = DB::select("
             SELECT min(result.seq_no) as min_seq_no
             FROM(".$main_query.") result");
 
+          //นับจำนวน emp ที่มีค่า seq_no น้อยที่สุด
           $CountMinSeqNo = DB::select("
             SELECT count(result.emp_id) as num_emp
             FROM(".$main_query."
@@ -577,6 +610,7 @@ class BonusReportController extends Controller
 
           if($CountMinSeqNo[0]->num_emp == 1){
 
+            // หา emp_id ของคนที่มี seq_no น้อยที่สุด
             $EmpMinSeqNo = DB::select("
               SELECT result.emp_id
               FROM(".$main_query."
@@ -587,12 +621,11 @@ class BonusReportController extends Controller
 
           } else {
 
-            $ManagerMinSeqNo = DB::select("
-              SELECT result.emp_id
-              FROM(".$main_query."
-                AND le.seq_no = ".$MinSeqNO[0]->min_seq_no."
-                ORDER BY emp.emp_id ASC
-              ) result
+            // หา emp_id ที่น้อยที่สุดที่มี seq_no น้อยที่สุด
+            $ManagerMinSeqNo = DB::select(
+              $main_query.
+              "AND le.seq_no = ".$MinSeqNO[0]->min_seq_no."
+              ORDER BY emp.emp_id ASC
               LIMIT 1 ");
 
             $EmpManagerID = $ManagerMinSeqNo[0]->emp_id;
