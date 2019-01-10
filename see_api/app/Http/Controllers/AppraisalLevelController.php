4a0619c2440f0fff2233bc971e27b99ee6c834d0
GJ-DARIS <34049335+GJ-DARIS@users.noreply.github.com>
2019-01-10 15:40:59 +0700

    [Edited]: Defect 211 หน้า Level
    หน้า Level เพิ่ม Field ต่อไปนี้ ในหน้า Add, Edit
    
    เพิ่ม field ต่อไปนี้ที่หน้า add, edit ของ Level
    
    appraisal_level.is_start_cal_bonus
    appraisal_level.is_start_cal_mpi
    appraisal_level.is_start_cal_raise

--- see_api/app/Http/Controllers/AppraisalLevelController.php
+++ see_api/app/Http/Controllers/AppraisalLevelController.php
@@ -40,7 +40,7 @@ class AppraisalLevelController extends Controller
 		// 	a.level_id ASC
 		// ");
 		$items = DB::select("
-			SELECT a.level_id, a.appraisal_level_name, a.seq_no, a.is_all_employee, a.district_flag, a.is_org, a.is_individual, a.is_active, a.parent_id, a.is_hr, a.is_self_assign, a.is_group_action, a.is_show_quality, a.no_weight, b.appraisal_level_name parent_level_name, a.default_stage_id
+			SELECT a.level_id, a.appraisal_level_name, a.seq_no, a.is_all_employee, a.district_flag, a.is_org, a.is_individual, a.is_active, a.parent_id, a.is_hr, a.is_self_assign, a.is_group_action, a.is_show_quality, a.no_weight, b.appraisal_level_name parent_level_name, a.default_stage_id,a.is_start_cal_bonus,a.is_start_cal_mpi,a.is_start_cal_raise
 			FROM appraisal_level a
 			left outer join appraisal_level b
 			on a.parent_id = b.level_id
@@ -133,6 +133,9 @@ class AppraisalLevelController extends Controller
 			'is_self_assign' => 'boolean',
 			'is_group_action' => 'boolean',
 			'is_show_quality' => 'boolean',
+			'is_start_cal_bonus' => 'boolean',
+			'is_start_cal_mpi' => 'boolean',
+			'is_start_cal_raise' => 'boolean',
 			'district_flag' => 'boolean',
 			'no_weight' => 'required|boolean',
 			'default_stage_id' => 'integer'
