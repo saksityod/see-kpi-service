d4c2caa93fa96293b7763e1319bbfd5d1f772bc4
Methee Suksngacharoen <msuksang@gmail.com>
2019-01-23 11:39:46 +0700

    [Added]: Added service for 360 Calculation

--- see_api/app/Http/routes.php
+++ see_api/app/Http/routes.php
@@ -501,6 +501,7 @@ Route::group(['middleware' => 'cors'], function()
 	Route::get('appraisal360/parameter/org_level_individual', 'Appraisal360Degree\AppraisalGroupController@org_level_list_individual');
 	Route::get('appraisal360/parameter/org_individual', 'Appraisal360Degree\AppraisalGroupController@org_individual');
 	Route::get('appraisal360/Calculate/Etl', 'Appraisal360Degree\AppraisalGroupController@calculate_etl');
+	Route::get('appraisal360/Calculate/Api', 'Appraisal360Degree\AppraisalGroupController@calculate_api');
 	Route::get('appraisal360/check_permission_popup/{emp_result_id}', 'Appraisal360Degree\AppraisalGroupController@check_permission_popup');
 
 
